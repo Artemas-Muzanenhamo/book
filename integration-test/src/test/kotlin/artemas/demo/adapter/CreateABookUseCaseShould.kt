0c6e8e0e528f3cdecd2508d6078091e370a48ee8
Artemas-Muzanenhamo <artemasseng@gmail.com>
2022-10-10 13:10:51 +0100

    remove redundant JPA test

--- integration-test/src/test/kotlin/artemas/demo/adapter/CreateABookUseCaseShould.kt
+++ integration-test/src/test/kotlin/artemas/demo/adapter/CreateABookUseCaseShould.kt
@@ -1,5 +1,6 @@
 package artemas.demo.adapter
 
+import artemas.demo.adapter.persistence.repository.BookRepository
 import artemas.demo.dto.BookDTO
 import artemas.demo.ports.CreateABookUseCase
 import org.assertj.core.api.Assertions.assertThat
@@ -14,13 +15,25 @@ class CreateABookUseCaseShould {
     private lateinit var createABookUseCase: CreateABookUseCase
     @Autowired
     private lateinit var logger: Logger
+    @Autowired
+    private lateinit var bookRepository: BookRepository
 
     @Test
     fun `It saves a book given a valid id, book name and valid isbn number`() {
         val book = BookDTO(id = 54325, bookName = "Great Expectations", isbnNumber = 9873459837534)
 
-        val createdBook = createABookUseCase.createABook(book)
+        val createdBook: BookDTO = createABookUseCase.createABook(book)
 
+        // Returned when Persisted
         assertThat(createdBook).isNotNull
+            .hasFieldOrPropertyWithValue("id", 54325L)
+            .hasFieldOrPropertyWithValue("bookName", "Great Expectations")
+            .hasFieldOrPropertyWithValue("isbnNumber", 9873459837534L)
+
+        // Persisted Values in the DB
+        val (id, bookName, isbnNumber) = bookRepository.findAll().first()
+        assertThat(id).isEqualTo(54325L)
+        assertThat(bookName).isEqualTo("Great Expectations")
+        assertThat(isbnNumber).isEqualTo(9873459837534L)
     }
 }
