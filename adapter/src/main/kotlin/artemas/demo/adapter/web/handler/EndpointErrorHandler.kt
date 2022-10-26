63270933d3fccd2109fb8d66d4a674f0b806183c
Artemas-Muzanenhamo <artemasseng@gmail.com>
2022-10-26 22:25:06 +0100

    amend EndpointErrorHandler

--- adapter/src/main/kotlin/artemas/demo/adapter/web/handler/EndpointErrorHandler.kt
+++ adapter/src/main/kotlin/artemas/demo/adapter/web/handler/EndpointErrorHandler.kt
@@ -5,14 +5,10 @@ import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.ResponseStatus
 
 @ControllerAdvice
 class EndpointErrorHandler {
     @ExceptionHandler
-    @ResponseStatus(BAD_REQUEST)
-    @ResponseBody
     fun handleBookIdNotFound(e: NotFoundException): ResponseEntity<String> {
         return ResponseEntity.status(BAD_REQUEST).body("An Invalid Book Id was supplied")
     }
