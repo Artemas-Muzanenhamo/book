ca0f19e2628305cb48df3bed9c600d3d7a59b8cd
Artemas-Muzanenhamo <artemasseng@gmail.com>
2022-11-15 09:31:45 +0000

    add test case when updating a non existent book

--- adapter/src/test/kotlin/artemas/demo/adapter/web/BookEndpointShould.kt
+++ adapter/src/test/kotlin/artemas/demo/adapter/web/BookEndpointShould.kt
@@ -6,9 +6,6 @@ import artemas.demo.ports.CreateABookUseCase
 import artemas.demo.ports.DeleteABookUseCase
 import artemas.demo.ports.UpdateABookUseCase
 import org.junit.jupiter.api.Test
-import org.mockito.BDDMockito.any
-import org.mockito.BDDMockito.doNothing
-import org.mockito.BDDMockito.doReturn
 import org.mockito.BDDMockito.given
 import org.slf4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
@@ -16,9 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
-import org.springframework.test.web.servlet.result.ContentResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -127,5 +122,26 @@ class BookEndpointShould {
                 }"""
                 )
             )
+    }
+
+    @Test
+    fun `Throws an InvalidBookException when trying to update a non existent book`() {
+        val book = BookDTO(id = 67832402348, bookName = "Kobe Bryant Documentary", isbnNumber = 836647362)
+        given(updateABookUseCase.update(book)).willThrow(NotFoundException("Book with id: 894379345 was not found"))
+
+        mockMvc.perform(
+            put("/book")
+                .accept(APPLICATION_JSON_VALUE)
+                .contentType(APPLICATION_JSON_VALUE)
+                .content(
+                    """
+                        {
+                            "id": 67832402348,
+                            "bookName": "Kobe Bryant Documentary",
+                            "isbnNumber": 836647362
+                        }
+                    """.trimIndent()
+                )
+        ).andExpect(status().isBadRequest).andExpect(content().string("An Invalid Book Id was supplied"))
     }
 }
